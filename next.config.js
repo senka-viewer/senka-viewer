2929db5c70021101b51a35739b9895d50ee5fea9
MZI <macchenjl@outlook.com>
2023-08-10 13:11:58 +0800

    全部升级

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 const cp = require('child_process');
 const { i18n } = require('./next-i18next.config.js')
 const withLess = require("next-with-less");
-const OptimizeCSSPlugin = require('css-minimizer-webpack-plugin');
 
 const pkgInfo = require('./package.json');
 
@@ -11,12 +10,5 @@ module.exports = withLess({
         APP_VERSION: pkgInfo.version,
         APP_COMMIT: cp.execSync('git rev-parse HEAD').toString().trim(),
         INNER_BASE_URL: process.env.INNER_BASE_URL || 'https://senka.me'
-    },
-    poweredByHeader: false,
-    webpack: (config, { dev }) => {
-        if (!dev) {
-            config.plugins.push(new OptimizeCSSPlugin());
-        }
-        return config
-    },
+    }
 });
